7b5c05dc53f35f1d488900c2cbba398a5f2dc07c
Salman <salmankps2001@gmail.com>
2023-12-12 12:46:11 +0530

    cors disabled

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const app = express();
-// const cors = require('cors');
+const cors = require('cors');
 const fs = require('fs').promises; // Using fs.promises for async file reading
 
 app.use(express.json());
-// app.use(cors());
+app.use(cors());
 
 const PORT = process.env.PORT || 3030;
 
