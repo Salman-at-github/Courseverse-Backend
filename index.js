17323298e2c6fae82b3f64bfe3be9331e38149ce
Salman <salmankps2001@gmail.com>
2023-12-13 19:47:37 +0530

    Student api added

--- index.js
+++ index.js
@@ -56,7 +56,35 @@ app.get('/api/courses/:id', async (req, res) => {
         res.status(500).json({ success: false, message: 'Internal Server Error' });
     }
 });
+app.get('/api/student/:id', async (req, res) => {
+    try {
+        const studentID = parseInt(req.params.id); // Convert the id parameter to an integer
+
+        // Assuming you have a JSON file named 'data.json'
+        const filePath = 'studentData.json';
+
+        // Read the contents of the JSON file asynchronously
+        const jsonData = await fs.readFile(filePath, 'utf-8');
+
+        // Parse the JSON data
+        const students = JSON.parse(jsonData);
+
+        // Find the course with the specified ID
+        const student = students.find(student => student.id === studentID);
+
+        if (student) {
+            // Send the found student as the response
+            res.status(200).json(student);
+        } else {
+            // If the student with the specified ID is not found, send a 404 response
+            res.status(404).json({ message: 'Student not found' });
+        }
+    } catch (error) {
+        console.error('Error:', error.message);
+        res.status(500).json({ success: false, message: 'Internal Server Error' });
+    }
+});
 
 app.listen(PORT, () => {
-    console.log("Server running now");
+    console.log(`Server running on port: ${PORT}`);
 });
---
+++ models/studentSchema.js
@@ -0,0 +1,5 @@
+const mongoose = require('mongoose')
+
+const StudentSchema = new mongoose.Schema({
+    
+})
